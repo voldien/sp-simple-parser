9a4113318ad3581f6ed3b79f90e50504790ce857
Voldie <voldiekami@gmail.com>
2017-09-07 16:23:43 +0200

    Added the init version of the setup script.

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import setup, find_packages
+
+if "install" in sys.argv[1]:
+
+    lib_paths = [get_python_lib()]
+    if lib_paths[0].startswith("/usr/lib"):
+        lib_paths.append(get_python_lib(prefix="/usr/local/"))
+    for path in lib_paths:
+        existing_path = os.path.abspath(os.path.join(path, "kcw"))
+        if os.path.exists(existing_path):
+            break
+
+version = "1.0a0"
+
+EXCLUDE_FROM_PACKAGES = ['']
+
+setup(
+    name="simpleparser",
+    version=version,
+    url="https://github.com/voldien/simpeparser",
+    author="Valdemar Lindberg",
+    author_email="voldiekami@gmail.com",
+    description=open('README.md').read(),
+    license="GNU General Public License v3 (GPLv3)",
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: SP',
+	'Intended Audience :: Developers'
+        'License :: OSI Approved ::  GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+)
+
